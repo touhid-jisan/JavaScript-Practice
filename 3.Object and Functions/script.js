c7bf05744b4a7308e7e03208e4c92ab846306f30
touhid-jisan <touhidulislamjisan@gmail.com>
2020-08-20 13:56:27 +0600

    Object.create

--- 3.Object and Functions/script.js
+++ 3.Object and Functions/script.js
@@ -6,7 +6,7 @@
 //	job: 'Teacher'
 //};
 //
-
+/*
 var Person = function(name, yearOfBirth, job) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
@@ -29,3 +29,52 @@ jane.calculateAge();
 console.log(jhon.lastName);
 console.log(mark.lastName);
 console.log(jane.lastName);
+*/
+
+//Object.create 
+
+var personProto = {
+	calculateAge: function() {
+		console.log(2020 - this.yearOfBirth);
+	}
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1980;
+john.job = 'Teacher';
+
+
+var jane = Object.create(personProto, {
+	name: {value: 'Jane'},
+	yearOfBirth: {value : 1969},
+	job: {value: 'Designer'}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
