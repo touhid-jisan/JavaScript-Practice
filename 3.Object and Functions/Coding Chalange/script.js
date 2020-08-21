2022fe02f861956fb261a4667ba0190d4c1f2643
touhid-jisan <touhidulislamjisan@gmail.com>
2020-08-21 17:47:46 +0600

    Coding Chalange Part 2

--- 3.Object and Functions/Coding Chalange/script.js
+++ 3.Object and Functions/Coding Chalange/script.js
@@ -11,7 +11,7 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-
+/*
 (function() {
 	
 	var q1 = new Question('Is JavaScript the coolest programming language in the world?',
@@ -52,6 +52,87 @@ c) correct answer (I would use a number for this)
 	console.log(answer);
 	ques[n].checkAnswer(answer);
 }());
+*/
+/*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function() {
+	
+	var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+						  ['Yes', 'No'],
+						  0);
+	var q2 = new Question('What is the name of this course\'s teacher?',
+						  ['John', 'Michel', 'Jonas'],
+						  2);
+	var q3 = new Question('What does best describe coding?',
+						  ['Boring', 'Hard', 'Fun', 'Tediuos'],
+                          2);
+	
+	var ques = [q1, q2, q3];
+	
+	function Question(question, answer, correct) {
+		this.question = question;
+		this.answer = answer;
+		this.correct = correct;
+	}
+	function score () {
+		var scr = 0;
+		return function(correct) {
+			if(correct) {
+				scr++;
+			}
+			return scr;
+		}
+	}
+	var keepScore = score();
+	
+	function nextQuestion() {
+		var n = Math.floor(Math.random() * ques.length);
+		ques[n].displayQuestion();
+		var answer = prompt('Please Select The Correct Answer!');
+		
+		if(answer !== 'exit') {
+			ques[n].checkAnswer(parseInt(answer), keepScore);
+			nextQuestion();	
+		}
+	}
+	
+	
+	// --------------Prototypes Start----------------
+	Question.prototype.displayQuestion = function() {
+		console.log(this.question);
+		for(var i = 0; i < this.answer.length; i++) {
+			console.log(i + ': ' + this.answer[i]);
+		}
+	}
+	
+	Question.prototype.checkAnswer = function (answer, callback) {
+		var scr;
+		if (answer 	=== this.correct) {
+			console.log('Correct Answer!');
+			scr = callback(true);
+		} else {
+			console.log(' Wrong Answer! Try Again!');
+			scr = callback(false);
+		}
+		this.displayScore(scr);
+	}
+	Question.prototype.displayScore = function(score) {
+		console.log('Your current score : ' + score);
+		console.log('-------------------------------')
+	}
+	// ----------------Prototypes End----------------------
+	
+	nextQuestion();
+	
+}());
+
+
 
 
 
