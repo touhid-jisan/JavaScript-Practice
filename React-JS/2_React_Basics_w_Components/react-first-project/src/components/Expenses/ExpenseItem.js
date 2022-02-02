44ffaa364d20400ba5bece7cfa2585e9756f7d1e
Touhidhul Islam Jisan <touhidulislamjisan@gmail.com>
2022-02-02 19:45:43 +0600

    Using the State Hook

--- React-JS/2_React_Basics_w_Components/react-first-project/src/components/Expenses/ExpenseItem.js
+++ React-JS/2_React_Basics_w_Components/react-first-project/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import React, { useState } from  'react';
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  // let title = props.title;
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
   return (
     <Card className="expense-item">
       {/* <div>{props.date.toISOString()}</div> */}
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Chagned Title</button>
     </Card>
   );
 }
